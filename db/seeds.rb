b7bc4b0fde48d495b7befd813a265056c8d29b3b
alvin <alvin.loh@2359media.com>
2013-06-10 22:57:16 +0800

    create user table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+u = User.new(:email => "alvintamie@gmail.com", :password => "admin1234")
+u.role = "admin"
+u.activated = true
+u.locked = false
+u.save!
+
+puts "you have created an admin user [admin@2359media.com] with password [admin2359]"
