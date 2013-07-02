2a9a8038a290ff699ae6dc9e56f0ce6e155ac511
Alvin <alvin.loh@2359media.com>
2013-07-02 22:45:52 +0800

    fix upvote bugs

--- app/assets/javascripts/student/testimonials.js
+++ app/assets/javascripts/student/testimonials.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
   $(".upvote").on("click",function(event){
     testimonial_id = this.getAttribute("tid");
+    tid = $(this).attr("tid");
     event.preventDefault();
       $.ajax({
          type: "GET",
          url: location.protocol + "//" + location.host + "/student/testimonials/" + testimonial_id + "/upvote",
          success: function(msg){
            if(msg.status == "success"){
-              n = parseInt($("#votes").text());
-              $("#votes").text(n+1);
+              n = parseInt($("#votes-"+tid).text());
+              $("#votes-"+tid).text(n+1);
            }
          }
        });
@@ -22,8 +23,8 @@ $(document).ready(function(){
          url: location.protocol + "//" + location.host + "/student/testimonials/" + testimonial_id +"/downvote",
          success: function(msg){
            if(msg.status == "success"){
-              n = parseInt($("#votes").text());
-              $("#votes").text(n-1);
+              n = parseInt($("#votes-"+tid).text());
+              $("#votes-"+tid).text(n-1);
            }
          }
        });
