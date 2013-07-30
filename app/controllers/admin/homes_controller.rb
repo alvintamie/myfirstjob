d72c2245b9e0460498e2f75f4320902c86bdaf29
alvi0010 <alvintamie@gmail.com>
2013-07-30 07:35:33 +0700

    what's different

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,6 +2,9 @@ class Admin::HomesController < ApplicationController
 	before_filter :authenticate_admin_user!
 
 	def index
-		
+		@events = Event.approveds.featureds.order("created_at DESC").limit(10)
+    	@testimonials = Testimonial.find(:all, :limit => 8, :conditions => { :status => "approved"},:order => "created_at DESC")
+    	@interviews = Interview.find(:all, :limit => 3, :order => "created_at DESC")
+    	@featured_company_details = CompanyDetail.featureds
 	end
 end
--- app/controllers/student/homes_controller.rb
+++ app/controllers/student/homes_controller.rb
@@ -3,7 +3,7 @@ class Student::HomesController < ApplicationController
 
 	def index
     	@events = Event.approveds.featureds.order("created_at DESC").limit(10)
-    	@testimonials = Testimonial.find(:all, :limit => 6, :order => "created_at DESC")
+    	@testimonials = Testimonial.find(:all, :limit => 8, :conditions => { :status => "approved"},:order => "created_at DESC")
     	@interviews = Interview.find(:all, :limit => 3, :order => "created_at DESC")
 	end 
 end
